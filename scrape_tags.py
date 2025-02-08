e5f63e8148b83c819dedd08b295c61f520ecef71
BetaDoggo <alexnunet@gmail.com>
2025-02-07 20:34:55 -0500

    add time travel

--- scrape_tags.py
+++ scrape_tags.py
@@ -1,17 +1,22 @@
 import os
 import requests
-import collections
 import csv
 import time
-
-class Complete(Exception): pass
+import datetime
 
 csv_filename = input('Output filename: ')
 minimum_count = input('Minimum tag count (> 50 is preferable): ')
 dashes = input('replace \'_\' with \'-\'? (often better for prompt following) (Y/n): ')
 exclude = input('enter categories to exclude: (general,artist,copyright,character,post) (press enter for none): \n')
-alias = input('Include aliases? (Only supported in tag-complete) (y/N): ')
+alias = input('Include aliases? (Only supported in tag-complete) (Y/n): ')
 boards = input('Enter boards to scrape danbooru(d), e621(e), both(de) (default: danbooru): ')
+date = input('Enter cutoff date. ex: 2024-09-03 for september 3rd 2024: ')
+try:
+    max_date =  datetime.datetime.strptime(date.strip()[:10], "%Y-%m-%d")
+    print(f"Using date: {max_date}")
+except:
+    max_date = str(datetime.datetime.now())[:10]
+    print(f"Using todays date: {max_date}")
 
 boards = boards.lower()
 if (not "d" in boards) and (not "e" in boards):
@@ -36,64 +41,61 @@ if not 'n' in dashes.lower():
     dashes = 'y'
     csv_filename += '-temp'
 
-if not 'y' in alias.lower():
-    alias = 'n'
+if not 'n' in alias.lower():
+    alias = 'y'
 
 if not minimum_count.isdigit():
     minimum_count = 50
 
-# Base URL without the page parameter
-base_url = 'https://danbooru.donmai.us/tags.json?limit=1000&search[hide_empty]=yes&search[is_deprecated]=no&search[order]=count'
-alias_url = 'https://danbooru.donmai.us/tag_aliases.json?commit=Search&limit=1000&search[order]=tag_count'
+# Base URLs
+dan_base_url = 'https://danbooru.donmai.us/tags.json?limit=1000&search[hide_empty]=yes&search[is_deprecated]=no&search[order]=count'
+dan_alias_url = 'https://danbooru.donmai.us/tag_aliases.json?commit=Search&search%5Bconsequent_name_matches%5D='
 e6_base_url = 'https://e621.net/tags.json?limit=1000&search[hide_empty]=yes&search[is_deprecated]=no&search[order]=count'
-e6_alias_url = 'https://e621.net/tag_aliases.json?commit=Search&limit=1000&search[order]=tag_count'
+e6_alias_url = 'https://e621.net/tag_aliases.json?commit=Search&search%5Bconsequent_name%5D='
 
-dan_aliases = collections.defaultdict(str)
-e6_aliases = collections.defaultdict(str)
+session = requests.Session()
 
-def get_aliases(alias_url,type):
-    # create alias dictionary
-    try:
-        aliases = collections.defaultdict(str)
-        for page in range(1,1001):
-            # Update the URL with the current page
-            url = f'{alias_url}&page={page}'
-            # Fetch the JSON data
-            response = requests.get(url,headers={"User-Agent": "tag-list/2.0"})
-            # Check if the request was successful
-            if response.status_code == 200:
-                data = response.json()
-                # Break the loop if the data is empty (no more tags to fetch)
-                if not data:
-                    print(f'No more data found at page {page}. Stopping.', flush=True)
-                    break
-                for item in data:
-                    if type == "e": # danbooru doesn't have post counts for aliases
-                        if int(item['post_count']) < int(minimum_count):
-                            raise Complete
-                    aliases[item['consequent_name']] += ',' + item['antecedent_name'] if aliases[item['consequent_name']] else item['antecedent_name']
-            print(f'Page {page} aliases processed.', flush=True)
-            # Sleep for 0.5 second because we have places to be
-            time.sleep(0.5)
-    except(Complete):
-        print("reached the post threshold")
-    return(aliases)
+class Complete(Exception): pass
 
-if alias == 'y':
-    if "d" in boards:
-        dan_aliases = get_aliases(alias_url,"d")
-    #if "e" in boards:
-    #    e6_aliases = get_aliases(e6_alias_url,"e")
+def get_aliases(tags, name, url, max_date, session):
+    url = url + name
+    while True:
+        response = session.get(url,headers={"User-Agent": "tag-list/3.0"})
+        if response.status_code == 200:
+            aliases = {}
+            data = response.json()
+            for item in data:
+                aliases[item['antecedent_name']] = item['antecedent_name'],item['created_at']
 
-#######
+            aliases = {key: value for key, value in aliases.items() 
+               if datetime.datetime.strptime(value[1][:10], "%Y-%m-%d") <= max_date}
+
+            if datetime.datetime.strptime(tags[name][2][:10], "%Y-%m-%d") >= max_date:
+                try:
+                    previous_key = tags.pop(name)
+                    tags[aliases[0][0]] = previous_key
+                    lst_alias = []
+                    for index in range(1, len(alias)):
+                        lst_alias += alias[index][0]
+                    tags[aliases[0][0]] += [lst_alias]
+                    dan_tags[aliases[0][0]] += [''] # safety index
+                    return
+                except: # if there are no aliases for a tag which must be removed
+                    print(f"Removed {name}")
+                    return
+            dan_tags[name] += [''] # safety index
+        else:
+            print("Failed to get aliases, likely a connection error.\nRetrying in 5 seconds...")  
+
+
 if "d" in boards:
     dan_tags = {}
     try:
         for page in range(1, 1001):
             # Update the URL with the current page
-            url = f'{base_url}&page={page}'
+            url = f'{dan_base_url}&page={page}'
             # Fetch the JSON data
-            response = requests.get(url,headers={"User-Agent": "tag-list/2.0"})
+            response = session.get(url,headers={"User-Agent": "tag-list/3.0"})
             # Check if the request was successful
             if response.status_code == 200:
                 data = response.json()
@@ -106,11 +108,8 @@ if "d" in boards:
                     if int(item['post_count']) < int(minimum_count): # break if below minimum count
                         raise Complete
                     if not str(item['category']) in excluded:
-                        if alias == 'n':
-                            dan_tags[item['name']] = [item['category'],item['post_count'],'']
-                        else:
-                            alt = dan_aliases.get(item['name']) if dan_aliases.get(item['name']) != None else ''
-                            dan_tags[item['name']] = [item['category'],item['post_count'],alt]
+                        dan_tags[item['name']] = [item['category'],item['post_count'],item['created_at']]
+                        get_aliases(dan_tags, item['name'], dan_alias_url, max_date, session)
             else:
                 print(f'Failed to fetch data for page {page}. HTTP Status Code: {response.status_code}', flush=True)
                 break
@@ -119,6 +118,7 @@ if "d" in boards:
             time.sleep(0.5)
     except(Complete):
         pass
+
 if "e" in boards:
     e6_tags = {}
     try:
@@ -126,7 +126,7 @@ if "e" in boards:
             # Update the URL with the current page
             url = f'{e6_base_url}&page={page}'
             # Fetch the JSON data
-            response = requests.get(url,headers={"User-Agent": "tag-list/2.0"})
+            response = session.get(url,headers={"User-Agent": "tag-list/3.0"})
             # Check if the request was successful
             if response.status_code == 200:
                 data = response.json()
@@ -139,30 +139,22 @@ if "e" in boards:
                     if int(item['post_count']) < int(minimum_count): # break if below minimum count
                         raise Complete
                     if not str(item['category']) in excluded:
-                        #if alias == 'n':
-                        e6_tags[item['name']] = [item['category'],item['post_count'],'']
-                        #else:
-                        #    alt = e6_aliases.get(item['name']) if e6_aliases.get(item['name']) != None else ''
-                        #    e6_tags[item['name']] = [item['category'],item['post_count'],alt]
+                        e6_tags[item['name']] = [item['category'],item['post_count'],item['created_at']]
+                        get_aliases(e6_tags, item['name'], e6_alias_url, max_date, session)
             else:
                 print(f'Failed to fetch data for page {page}. HTTP Status Code: {response.status_code}', flush=True)
                 break
-            print(f'e621 page {page} processed.', flush=True)
+            print(f'e6 page {page} processed.', flush=True)
             # Sleep for 0.5 second because we have places to be
             time.sleep(0.5)
-    except Complete:
-        print(f'All tags with {minimum_count} posts or greater have been scraped.')
+    except(Complete):
+        pass
 
 # Merge boards
 if ("d" in boards) and ("e" in boards):
     for tag in dan_tags:
         if tag in e6_tags:
             e6_tags[tag][1] += dan_tags[tag][1] # combined count
-            """if e6_tags[tag][2] != None and dan_tags[tag][2] != None:
-                if e6_tags[tag][2] == "":
-                    e6_tags[tag][2] += dan_tags[tag][2]  # aliases
-                else:
-                    e6_tags[tag][2] += "," + dan_tags[tag][2]"""
     dan_tags.update(e6_tags)
     full_tags = dan_tags
 elif "d" in boards:
@@ -170,7 +162,6 @@ elif "d" in boards:
 else:
     full_tags = e6_tags
 
-# Open a file to write
 print("writing to file")
 with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
@@ -181,24 +172,22 @@ with open(csv_filename, mode='w', newline='', encoding='utf-8') as file:
             if alias == 'n':
                 writer.writerow([key,value[0],value[1],''])
             else:
-                writer.writerow([key,value[0],value[1],value[2]])
+                writer.writerow([key,value[0],value[1],value[3]])
     # Explicitly flush the data to the file
     file.close()
 
-    if dashes == 'y':
-        print(f'Replacing \'_\' with \'-\'')
-        with open(csv_filename, 'r', encoding='utf-8') as csvfile:
-            reader = csv.reader(csvfile)
-            with open(csv_filename.removesuffix('-temp'), 'w', encoding='utf-8', newline='') as outfile:
-                writer = csv.writer(outfile)
-                for row in reader:
-                    if not row[0] in kaomojis:
-                        row[0] = row[0].replace("_", "-")
-                        row[3] = row[3].replace("_", "-")
-                    writer.writerow(row)
-                outfile.close()    
-            csvfile.close()
-        os.remove(csv_filename)
-        csv_filename = csv_filename.removesuffix('-temp')
-
-print(f'Data has been written to {csv_filename}', flush=True)
+if dashes == 'y':
+    print(f'Replacing \'_\' with \'-\'')
+    with open(csv_filename, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        with open(csv_filename.removesuffix('-temp'), 'w', encoding='utf-8', newline='') as outfile:
+            writer = csv.writer(outfile)
+            for row in reader:
+                if not row[0] in kaomojis:
+                    row[0] = row[0].replace("_", "-")
+                    row[3] = row[3].replace("_", "-")
+                writer.writerow(row)
+            outfile.close()    
+        csvfile.close()
+    os.remove(csv_filename)
+    csv_filename = csv_filename.removesuffix('-temp')
